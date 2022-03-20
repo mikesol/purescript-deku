a0a1c77c6ee837ad90dc5badac4159d077311a83
mikesol <mikesol@users.noreply.github.com>
2022-03-20 04:28:47 +0000

    deploy: 9ab52af1040b091286c62ef79c0c5c161c17cb42

--- HelloWorld.purs
+++ HelloWorld.purs
@@ -9,7 +9,7 @@ import Deku.Control.Functions.Graph (iloop, (@!>))
 import Deku.Control.Types (Frame0, Scene)
 import Deku.Create (icreate)
 import Deku.Graph.Attribute (Cb(..))
-import Deku.Graph.DOM (OnClick(..), Href_(..), a, a'attr, root, text, (:=))
+import Deku.Graph.DOM (OnClick(..), Href(..), a, a'attr, root, text, (:=))
 import Deku.Interpret (makeFFIDOMSnapshot)
 import Deku.Run (RunDOM, RunEngine, TriggeredScene, defaultOptions, run)
 import Effect (Effect)
@@ -28,7 +28,7 @@ scene elt =
       ( icreate $
           root elt
             { hello: text "click "
-            , helloA: a [ Href_ := "#", OnClick := Cb (const $ push false) ]
+            , helloA: a [ Href := "#", OnClick := Cb (const $ push false) ]
                 { world: text "me" }
             }
       )
