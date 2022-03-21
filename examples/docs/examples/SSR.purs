f28e8e9e27efd9f282fd5c73d5a8ba27bab8d0e0
Mike Solomon <mike@wavr.so>
2022-03-21 21:15:34 +0200

    Adds SSR example

---
+++ examples/docs/examples/SSR.purs
@@ -0,0 +1,65 @@
+module Deku.Example.Docs.Example.Component where
+
+import Prelude
+
+import Data.Either (Either(..))
+import Data.Foldable (for_)
+import Data.Maybe (maybe)
+import Data.Tuple.Nested ((/\))
+import Deku.Control.Functions.Graph (freeze, (@!>))
+import Deku.Control.Types (Frame0, Scene, oneFrame)
+import Deku.Create (icreate)
+import Deku.Graph.DOM (root)
+import Deku.Graph.DOM as D
+import Deku.Graph.DOM.Shorthand as S
+import Deku.Interpret (class DOMInterpret, makeFFIDOMSnapshot)
+import Deku.Run (defaultOptions, run)
+import Deku.SSR (ssr, toXML)
+import Deku.Util (detup)
+import Effect (Effect)
+import FRP.Event (subscribe)
+import Web.DOM (Element)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (body)
+import Web.HTML.HTMLElement (toElement)
+import Web.HTML.Window (document)
+
+scene
+  :: forall env dom engine push
+   . DOMInterpret dom engine
+  => Element
+  -> Scene env dom engine Frame0 push Unit
+scene elt =
+  ( \_ _ ->
+      ( icreate $ root elt
+          ( detup $ D.p [] (S.text "Here is some XML!") /\ D.pre []
+              ( S.code []
+                  ( S.text
+                      ( maybe "" toXML
+                          ( ssr
+                              ( map ((#) unit)
+                                  ( oneFrame
+                                      (scene elt)
+                                      (Left unit)
+                                      (const $ pure unit)
+                                  ).instructions
+                              )
+                          )
+                      )
+                  )
+              ) /\ unit
+          )
+      )
+  ) @!> freeze
+
+main :: Effect Unit
+main = do
+  b' <- window >>= document >>= body
+  for_ (toElement <$> b') \elt -> do
+    ffi <- makeFFIDOMSnapshot
+    subscribe
+      ( run (pure unit) (pure unit) defaultOptions ffi
+          (scene elt)
+
+      )
+      (_.res >>> pure)
