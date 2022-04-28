8d3a75752e70085c4058e3595ac9adb8ca711ff8
Mike Solomon <mike120982@gmail.com>
2022-04-28 17:43:46 +0300

    Article

---
+++ article/Behavior.purs
@@ -0,0 +1,33 @@
+module Main where
+
+import Prelude
+
+import Control.Alt ((<|>))
+import Data.Tuple (Tuple(..))
+import Deku.Control (plant, text, text_)
+import Deku.DOM as D
+import Deku.Toplevel (runInBody1)
+import Effect (Effect)
+import Effect.Random as Random
+import FRP.Behavior (Behavior, behavior, sample_)
+import FRP.Event (bang, fold, makeEvent, memoize, subscribe)
+import FRP.Event.Time (interval)
+
+random :: Behavior Number
+random = behavior \e ->
+  makeEvent \k -> subscribe e \f ->
+    Random.random >>= k <<< f
+
+example counter = D.div_
+  [ text_ "Here are some random numbers"
+  , text (show <$> sample_ (Tuple <$> random <*> random) counter)
+  ]
+
+main :: Effect Unit
+main = runInBody1
+  ( memoize
+      ( (fold (const (add 1)) (interval 400) 0 <|> bang 0)
+      )
+      (plant <<< example)
+
+  )
--- article/KeepLatest.purs
+++ article/KeepLatest.purs
@@ -9,13 +9,14 @@ import Deku.Control (blank, plant)
 import Deku.DOM as D
 import Deku.Toplevel (runInBody1)
 import Effect (Effect)
-import FRP.Event (bang, bus, fold, keepLatest, memoize)
+import FRP.Event (bang, fold, keepLatest, memoize)
 import FRP.Event.Time (interval)
 
+bgColor :: Int -> String
 bgColor i =
    "background-color:rgb(255,255," <> show (mod i 256) <> ")"
 
-example counter = bus \push event -> plant $ D.div_
+example counter = D.div_
   [ D.input
       ( keepLatest $
           ( \i -> oneOfMap bang
