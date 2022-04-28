de5e2dd44d09acf8de61aee866a8436f622681d3
Mike Solomon <mike120982@gmail.com>
2022-04-28 18:08:59 +0300

    Adds fix

--- article/KeepLatest.purs
+++ article/KeepLatest.purs
@@ -5,22 +5,23 @@ import Prelude
 import Control.Alt ((<|>))
 import Data.Foldable (oneOfMap)
 import Deku.Attribute ((:=))
-import Deku.Control (blank, plant)
+import Deku.Control (blank, plant, text_)
 import Deku.DOM as D
 import Deku.Toplevel (runInBody1)
 import Effect (Effect)
 import FRP.Event (bang, fold, keepLatest, memoize)
 import FRP.Event.Time (interval)
 
-bgColor :: Int -> String
-bgColor i =
-   "background-color:rgb(255,255," <> show (mod i 256) <> ")"
+padMe :: Int -> String
+padMe i =
+  "margin: " <> show (mod i 256) <> "px;"
 
 example counter = D.div_
-  [ D.input
+  [ text_ "Hello margin"
+  , D.input
       ( keepLatest $
           ( \i -> oneOfMap bang
-              [ D.Style := bgColor i
+              [ D.Style := padMe i
               , D.Xtype := "checkbox"
               , D.Checked := show (if mod i 2 == 0 then true else false)
               ]
