545ca5d2e3c1f2e582fcd259a3165672e927e5c8
Mike Solomon <mike120982@gmail.com>
2022-04-28 17:36:11 +0300

    Adds keep latest

---
+++ article/KeepLatest.purs
@@ -0,0 +1,37 @@
+module Main where
+
+import Prelude
+
+import Control.Alt ((<|>))
+import Data.Foldable (oneOfMap)
+import Deku.Attribute ((:=))
+import Deku.Control (blank, plant)
+import Deku.DOM as D
+import Deku.Toplevel (runInBody1)
+import Effect (Effect)
+import FRP.Event (bang, bus, fold, keepLatest, memoize)
+import FRP.Event.Time (interval)
+
+bgColor i =
+   "background-color:rgb(255,255," <> show (mod i 256) <> ")"
+
+example counter = bus \push event -> plant $ D.div_
+  [ D.input
+      ( keepLatest $
+          ( \i -> oneOfMap bang
+              [ D.Style := bgColor i
+              , D.Checked := show (if mod i 2 == 0 then true else false)
+              ]
+          ) <$> counter
+      )
+      blank
+  ]
+
+main :: Effect Unit
+main = runInBody1
+  ( memoize
+      ( (fold (const (add 1)) (interval 400) 0 <|> bang 0)
+      )
+      (plant <<< example)
+
+  )
