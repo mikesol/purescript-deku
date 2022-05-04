7b5ec050f476422b5ffdd577d7e63c4896442f07
Mike Solomon <mike120982@gmail.com>
2022-05-04 11:40:42 +0300

    New helper functions for click and slider

---
+++ src/Deku/Listeners.purs
@@ -0,0 +1,42 @@
+module Deku.Listeners where
+
+import Prelude
+
+import Control.Alt (alt)
+import Data.Foldable (for_)
+import Deku.Attribute (class Attr, Attribute, attr, cb, (:=))
+import Deku.DOM as D
+import Effect (Effect)
+import FRP.Event (Event, bang)
+import Web.Event.Event (target)
+import Web.HTML.HTMLInputElement (fromEventTarget, valueAsNumber)
+
+click
+  :: forall event cb element
+   . Functor event
+  => Attr element D.OnClick cb
+  => event cb
+  -> event (Attribute element)
+click = map (attr D.OnClick)
+
+click_
+  :: forall m event cb element
+   . Functor event
+  => Monoid m
+  => Attr element D.OnClick cb
+  => event (m -> cb)
+  -> event (Attribute element)
+click_ = map (attr D.OnClick <<< (_ $ mempty))
+
+slider :: Event (Number -> Effect Unit) -> Event (Attribute D.Input_)
+slider = alt (bang $ D.Xtype := "range") <<< map
+  ( \push ->
+      D.OnInput := cb \e -> for_
+        ( target e
+            >>= fromEventTarget
+        )
+        ( valueAsNumber
+            >=> push
+        )
+
+  )
