2470524414a2c78f856d6715d1ea96ca4c108342
Mike Solomon <mike120982@gmail.com>
2022-05-10 09:46:19 +0300

    Adds foreign impl

---
+++ src/Deku/SSR.js
@@ -0,0 +1,37 @@
+export const doPursxReplacements = ({ id, html, pxScope, cache, verb }) => {
+	let vhtml = html;
+	const entries = Object.entries(cache);
+	for (var i = 0; i < entries.length; i++) {
+		const key = entries[i][0];
+		if (entries[i][1] === true) {
+			// it is an attribute
+			vhtml = vhtml.replace(
+				verb + key + verb,
+				"data-deku-attr-internal=" +
+					'"' +
+					key +
+					'" data-deku-ssr-' +
+					key +
+					pxScope +
+					'="true"'
+			);
+		} else {
+			vhtml = vhtml.replace(
+				verb + key + verb,
+				'<span style="display:contents;" data-deku-elt-internal=' +
+					'"' +
+					key +
+					'" data-deku-ssr-' +
+					key +
+					pxScope +
+					'="true"></span>'
+			);
+		}
+	}
+	const gtix = vhtml.indexOf(">");
+	const cix = vhtml.indexOf("/>");
+	const ix = cix == gtix - 1 ? cix : gtix;
+	return (
+		vhtml.slice(0, ix) + " data-deku-ssr-" + id + '="true" ' + vhtml.slice(ix)
+	);
+};
