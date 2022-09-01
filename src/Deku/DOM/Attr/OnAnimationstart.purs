0b4afe4c7eb13e8cf8b9f28a813392e1e32976bd
Mike Solomon <mike120982@gmail.com>
2022-09-01 12:35:37 +0300

    Effn' files...

---
+++ src/Deku/DOM/Attr/OnAnimationstart.purs
@@ -0,0 +1,14 @@
+module Deku.DOM.Attr.OnAnimationstart  where
+
+import Prelude
+import Effect (Effect)
+import Deku.Attribute (class Attr, Cb(..), cb', unsafeAttribute)
+
+
+data OnAnimationstart  = OnAnimationstart 
+instance Attr anything OnAnimationstart  Cb where
+  attr OnAnimationstart  value = unsafeAttribute { key: "animationstart ", value: cb' value }
+instance Attr anything OnAnimationstart  (Effect Unit) where
+  attr OnAnimationstart  value = unsafeAttribute { key: "animationstart ", value: cb' (Cb (const (value $> true))) }
+instance Attr anything OnAnimationstart  (Effect Boolean) where
+  attr OnAnimationstart  value = unsafeAttribute { key: "animationstart ", value: cb' (Cb (const value)) }
---
+++ src/Deku/DOM/Attr/OnTouchcancel.purs
@@ -0,0 +1,14 @@
+module Deku.DOM.Attr.OnTouchcancel   where
+
+import Prelude
+import Effect (Effect)
+import Deku.Attribute (class Attr, Cb(..), cb', unsafeAttribute)
+
+
+data OnTouchcancel   = OnTouchcancel  
+instance Attr anything OnTouchcancel   Cb where
+  attr OnTouchcancel   value = unsafeAttribute { key: "touchcancel  ", value: cb' value }
+instance Attr anything OnTouchcancel   (Effect Unit) where
+  attr OnTouchcancel   value = unsafeAttribute { key: "touchcancel  ", value: cb' (Cb (const (value $> true))) }
+instance Attr anything OnTouchcancel   (Effect Boolean) where
+  attr OnTouchcancel   value = unsafeAttribute { key: "touchcancel  ", value: cb' (Cb (const value)) }
