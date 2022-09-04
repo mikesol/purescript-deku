6ba6a2f48068aafec8c76083b5e9b1122b19b4f8
Mike Solomon <mike120982@gmail.com>
2022-09-04 07:54:27 +0300

    Adds idempotency checks to all FFI functions

--- src/Deku/Interpret.js
+++ src/Deku/Interpret.js
@@ -36,99 +36,94 @@ export const attributeParent_ = (runOnJust) => (a) => (state) => () => {
 };
 
 export const makeElement_ = (runOnJust) => (tryHydration) => (a) => (state) => () => {
-	if (state.units[a.id]) {
-		var dom;
-		var ptr = a.id;
-		if (!state.scopes[a.scope]) {
-			state.scopes[a.scope] = [];
-		}
-		state.scopes[a.scope].push(ptr);
-		// note that, for portals, this will be broken in its current form
-		const iRan = runOnJust(a.parent)(() => () => {
-			if (
-				state.hydrating &&
-				tryHydration &&
-				(dom = document.body
-					.querySelectorAll("[data-deku-ssr-" + ptr + "]")
-					.item(0))
-			) {
-				state.units[ptr] = {
-					listeners: {},
-					parent: a.parent,
-					scope: a.scope,
-					main: dom,
-				};
-				return true;
-			}
-			return false;
-		})();
-		if (!iRan) {
+	var dom;
+	var ptr = a.id;
+	if (!state.scopes[a.scope]) {
+		state.scopes[a.scope] = [];
+	}
+	state.scopes[a.scope].push(ptr);
+	// note that, for portals, this will be broken in its current form
+	const iRan = runOnJust(a.parent)(() => () => {
+		if (
+			state.hydrating &&
+			tryHydration &&
+			(dom = document.body
+				.querySelectorAll("[data-deku-ssr-" + ptr + "]")
+				.item(0))
+		) {
 			state.units[ptr] = {
 				listeners: {},
 				parent: a.parent,
 				scope: a.scope,
-				main: document.createElement(a.tag),
+				main: dom,
 			};
+			return true;
 		}
+		return false;
+	})();
+	if (!iRan) {
+		state.units[ptr] = {
+			listeners: {},
+			parent: a.parent,
+			scope: a.scope,
+			main: document.createElement(a.tag),
+		};
 	}
 };
 export const makeText_ = (runOnJust) => (tryHydration) => (maybe) => (a) => (state) => () => {
-	if (state.units[a.id]) {
-
-		var ptr = a.id;
-		var dom;
-		if (!state.scopes[a.scope]) {
-			state.scopes[a.scope] = [];
-		}
-		state.scopes[a.scope].push(ptr);
-		// for SSR, we need a parent, otherwise we cannot get the child node
-		// note that, for portals, this will be broken in its current form
-		const iRan = runOnJust(a.parent)((parent) => () => {
-			if (
-				state.hydrating &&
-				tryHydration &&
+	var ptr = a.id;
+	var dom;
+	if (!state.scopes[a.scope]) {
+		state.scopes[a.scope] = [];
+	}
+	state.scopes[a.scope].push(ptr);
+	// for SSR, we need a parent, otherwise we cannot get the child node
+	// note that, for portals, this will be broken in its current form
+	const iRan = runOnJust(a.parent)((parent) => () => {
+		if (
+			state.hydrating &&
+			tryHydration &&
+			// hack
+			(dom = document.body
 				// hack
-				(dom = document.body
-					// hack
-					.querySelectorAll("[data-deku-ssr-" + parent + "]")
-					.item(0))
-			) {
-				var i = 0;
-				// if the length is one, that means that there is only a comment because
-				// the text is empty
-				// so we need to add a text node
-				if (dom.childNodes.length === 1) {
-					dom.prepend(document.createTextNode(""));
-				} else {
-					for (var i = 0; i < dom.childNodes.length; i++) {
-						if (
-							dom.childNodes[i].nodeType === 8 &&
-							dom.childNodes[i].nodeValue === ptr
-						) {
-							i = i - 1;
-							break;
-						}
+				.querySelectorAll("[data-deku-ssr-" + parent + "]")
+				.item(0))
+		) {
+			var i = 0;
+			// if the length is one, that means that there is only a comment because
+			// the text is empty
+			// so we need to add a text node
+			if (dom.childNodes.length === 1) {
+				dom.prepend(document.createTextNode(""));
+			} else {
+				for (var i = 0; i < dom.childNodes.length; i++) {
+					if (
+						dom.childNodes[i].nodeType === 8 &&
+						dom.childNodes[i].nodeValue === ptr
+					) {
+						i = i - 1;
+						break;
 					}
 				}
-				state.units[ptr] = {
-					// if we've done ssr for a text node, it will be a span,
-					// so we want to get the child node
-					main: dom.childNodes[i],
-					parent: a.parent,
-					scope: a.scope,
-				};
-				return true;
 			}
-			return false;
-		})();
-		if (!iRan) {
 			state.units[ptr] = {
-				main: document.createTextNode(""),
+				// if we've done ssr for a text node, it will be a span,
+				// so we want to get the child node
+				main: dom.childNodes[i],
 				parent: a.parent,
 				scope: a.scope,
 			};
-			connectXToY_(maybe, ptr, a.parent, state);
+			return true;
 		}
+		return false;
+	})();
+	if (!iRan) {
+		state.units[ptr] = {
+			main: document.createTextNode(""),
+			parent: a.parent,
+			scope: a.scope,
+		};
+		connectXToY_(maybe, ptr, a.parent, state);
 	}
 };
 
@@ -226,102 +221,99 @@ export const setText_ = (a) => (state) => () => {
 };
 
 export const makePursx_ = (runOnJust) => (tryHydration) => (maybe) => (a) => (state) => () => {
-	if (state.units[a.id]) {
-
-		var dom;
-		var tmp;
-		var ptr = a.id;
-		var html = a.html;
-		var verb = a.verb;
-		var cache = a.cache;
-		var parent = a.parent;
-		var scope = a.scope;
-		var pxScope = a.pxScope;
-		// note that, for portals, this will be broken in its current form
-		const iRan = runOnJust(a.parent)(() => () => {
-			if (
-				state.hydrating &&
-				tryHydration &&
-				// hack
-				(dom = document.body
-					.querySelectorAll("[data-deku-ssr-" + ptr + "]")
-					.item(0))
-			) {
-				state.units[ptr] = {
-					listeners: {},
-					scope: scope,
-					parent: parent,
-					main: dom,
-				};
-				return true;
-			}
-			return false;
-		})();
-		if (!iRan) {
-			const entries = Object.entries(cache);
-			for (var i = 0; i < entries.length; i++) {
-				const key = entries[i][0];
-				if (entries[i][1] === true) {
-					// it is an attribute
-					html = html.replace(
-						verb + key + verb,
-						"data-deku-attr-internal=" + '"' + key + '"'
-					);
-				} else {
-					html = html.replace(
-						verb + key + verb,
-						'<span style="display:contents;" data-deku-elt-internal=' +
-						'"' +
-						key +
-						'"></span>'
-					);
-				}
-			}
-			tmp = document.createElement("div");
-			tmp.innerHTML = html.trim();
+	var dom;
+	var tmp;
+	var ptr = a.id;
+	var html = a.html;
+	var verb = a.verb;
+	var cache = a.cache;
+	var parent = a.parent;
+	var scope = a.scope;
+	var pxScope = a.pxScope;
+	// note that, for portals, this will be broken in its current form
+	const iRan = runOnJust(a.parent)(() => () => {
+		if (
+			state.hydrating &&
+			tryHydration &&
+			// hack
+			(dom = document.body
+				.querySelectorAll("[data-deku-ssr-" + ptr + "]")
+				.item(0))
+		) {
 			state.units[ptr] = {
 				listeners: {},
 				scope: scope,
 				parent: parent,
-				main: tmp.firstChild,
+				main: dom,
 			};
+			return true;
 		}
-		if (!state.scopes[scope]) {
-			state.scopes[scope] = [];
+		return false;
+	})();
+	if (!iRan) {
+		const entries = Object.entries(cache);
+		for (var i = 0; i < entries.length; i++) {
+			const key = entries[i][0];
+			if (entries[i][1] === true) {
+				// it is an attribute
+				html = html.replace(
+					verb + key + verb,
+					"data-deku-attr-internal=" + '"' + key + '"'
+				);
+			} else {
+				html = html.replace(
+					verb + key + verb,
+					'<span style="display:contents;" data-deku-elt-internal=' +
+					'"' +
+					key +
+					'"></span>'
+				);
+			}
 		}
-		state.scopes[scope].push(ptr);
-		// we were hydrating if tmp is not defined
-		if (!tmp) {
-			// need to test if this will work in all cases! especially when we're setting
-			// attributes of the top-level element
-			tmp = dom;
-		}
-		tmp.querySelectorAll("[data-deku-attr-internal]").forEach(function (e) {
-			var key = e.getAttribute("data-deku-attr-internal");
-			// each individual unit has the name of the key plus its scope
-			// this is necessary to avoid namespacing conflicts
-			// in case multiple purs-x contain the same key
-			const namespacedKey = key + "@!%" + pxScope;
-			state.units[namespacedKey] = {
-				listeners: {},
-				main: e,
-				scope: scope,
-			};
-			state.scopes[scope].push(namespacedKey);
-		});
-		tmp.querySelectorAll("[data-deku-elt-internal]").forEach(function (e) {
-			var key = e.getAttribute("data-deku-elt-internal");
-			const namespacedKey = key + "@!%" + pxScope;
-			state.units[key + "@!%" + pxScope] = {
-				listeners: {},
-				main: e,
-				scope: scope,
-			};
-			state.scopes[scope].push(namespacedKey);
-		});
-		if (!dom) {
-			connectXToY_(maybe, ptr, parent, state);
-		}
+		tmp = document.createElement("div");
+		tmp.innerHTML = html.trim();
+		state.units[ptr] = {
+			listeners: {},
+			scope: scope,
+			parent: parent,
+			main: tmp.firstChild,
+		};
+	}
+	if (!state.scopes[scope]) {
+		state.scopes[scope] = [];
+	}
+	state.scopes[scope].push(ptr);
+	// we were hydrating if tmp is not defined
+	if (!tmp) {
+		// need to test if this will work in all cases! especially when we're setting
+		// attributes of the top-level element
+		tmp = dom;
+	}
+	tmp.querySelectorAll("[data-deku-attr-internal]").forEach(function (e) {
+		var key = e.getAttribute("data-deku-attr-internal");
+		// each individual unit has the name of the key plus its scope
+		// this is necessary to avoid namespacing conflicts
+		// in case multiple purs-x contain the same key
+		const namespacedKey = key + "@!%" + pxScope;
+		state.units[namespacedKey] = {
+			listeners: {},
+			main: e,
+			scope: scope,
+		};
+		state.scopes[scope].push(namespacedKey);
+	});
+	tmp.querySelectorAll("[data-deku-elt-internal]").forEach(function (e) {
+		var key = e.getAttribute("data-deku-elt-internal");
+		const namespacedKey = key + "@!%" + pxScope;
+		state.units[key + "@!%" + pxScope] = {
+			listeners: {},
+			main: e,
+			scope: scope,
+		};
+		state.scopes[scope].push(namespacedKey);
+	});
+	if (!dom) {
+		connectXToY_(maybe, ptr, parent, state);
 	}
 };
 
@@ -369,7 +361,6 @@ export const deleteFromCache_ = (a) => (state) => () => {
 
 export const sendToPos_ = (a) => (state) => () => {
 	if (state.units[a.id]) {
-
 		var ptr = a.id;
 		var pos = a.pos;
 		var parent = state.units[ptr].main.parentNode;
