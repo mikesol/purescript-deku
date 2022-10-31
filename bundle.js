e66953dd025883ec9e0a5a867f8540a35d3bff17
mikesol <mikesol@users.noreply.github.com>
2022-10-31 11:14:00 +0000

    deploy: e6f62f653a972673c6583d48172c605aeeb0a215

--- bundle.js
+++ bundle.js
@@ -348,28 +348,28 @@ import Prelude
 
 import Deku.Attribute ((:=))
 import Deku.Control (text_)
-import Deku.Core (fixed)
 import Deku.DOM as D
 import Deku.Toplevel (runInBody)
 import Effect (Effect)
 
 main :: Effect Unit
 main = runInBody
-  (fixed [ D.button_ [ text_ "I do nothing" ]
-    , D.ul_ $ map (D.li_ <<< pure <<< text_) [ "A", "B", "C" ]
-    , D.div_
-        [ D.a (pure $ D.Href := "https://example.com")
-            [ text_ "foo " ]
-        , D.i_ [ text_ " bar " ]
-        , D.span (pure $ D.Style := "font-weight: 800;")
-            [ text_ " baz" ]
-        ]
-    , D.div_
-        [ D.div_
-            [ D.div_ [ D.input (pure $ D.Xtype := "range") [] ]
-            ]
-        ]
-    ]
+  ( D.div_
+      [ D.button_ [ text_ "I do nothing" ]
+      , D.ul_ $ map (D.li_ <<< pure <<< text_) [ "A", "B", "C" ]
+      , D.div_
+          [ D.a (pure $ D.Href := "https://example.com")
+              [ text_ "foo " ]
+          , D.i_ [ text_ " bar " ]
+          , D.span (pure $ D.Style := "font-weight: 800;")
+              [ text_ " baz" ]
+          ]
+      , D.div_
+          [ D.div_
+              [ D.div_ [ D.input (pure $ D.Xtype := "range") [] ]
+              ]
+          ]
+      ]
   )`,Pp=`module Main where
 
 import Prelude
