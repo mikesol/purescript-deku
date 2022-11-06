74c9e4e2b444c5d162e3b0152330ff4861d272fb
Mike Solomon <mike120982@gmail.com>
2022-11-06 20:40:29 +0200

    Adds value for textarea

--- src/Deku/DOM/Attr/Value.purs
+++ src/Deku/DOM/Attr/Value.purs
@@ -1,15 +1,19 @@
 module Deku.DOM.Attr.Value where
 
+import Deku.Attribute (class Attr, prop', unsafeAttribute)
+import Deku.DOM (Textarea_)
 import Deku.DOM.Elt.Button (Button_)
 import Deku.DOM.Elt.Input (Input_)
 import Deku.DOM.Elt.Li (Li_)
 import Deku.DOM.Elt.Meter (Meter_)
 import Deku.DOM.Elt.Option (Option_)
-import Deku.DOM.Elt.Progress (Progress_)
 import Deku.DOM.Elt.Param (Param_)
-import Deku.Attribute (class Attr, prop', unsafeAttribute)
+import Deku.DOM.Elt.Progress (Progress_)
 
 data Value = Value
+
+instance Attr Textarea_ Value String where
+  attr Value value = unsafeAttribute { key: "value", value: prop' value }
 
 instance Attr Button_ Value String where
   attr Value value = unsafeAttribute { key: "value", value: prop' value }
