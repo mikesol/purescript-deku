abfa5cdf129bb8fa8147fda7fe64c8a4e58d8d88
Mike Solomon <mike120982@gmail.com>
2022-11-30 09:52:41 +0200

    Removes discard warning

--- src/Deku/Do.purs
+++ src/Deku/Do.purs
@@ -8,5 +8,6 @@ import Prelude hiding (bind, discard)
 bind :: forall a r q. ((a -> r) -> q) -> (a -> r) -> q
 bind f a = f a
 
-discard :: forall r q. ((Unit -> r) -> q) -> (Unit -> r) -> q
+-- we do not use any warnings for discard
+discard :: forall a r q. ((a -> r) -> q) -> (a -> r) -> q
 discard = bind
