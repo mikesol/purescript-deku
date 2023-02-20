24201fc6e694bc50824d5b737dbd237c74fb5dcd
Mike Solomon <mike@meeshkan.com>
2023-02-20 13:39:10 +0200

    Adds docs for Deku.Core

--- src/Deku/Control.purs
+++ src/Deku/Control.purs
@@ -1,10 +1,13 @@
+-- | This module contains various control structures for things like 
+-- | switching between elements, creating portals allowing elements to fly around
+-- | the DOM, and basic text creation. It also has several internal functions used for
+-- | DOM element creation.
 module Deku.Control
   ( (<#~>)
   , (<$~>)
   , blank
   , deku
   , elementify
-  , ezDyn
   , globalPortal
   , globalPortal1
   , guard
@@ -34,8 +37,7 @@ import Data.Profunctor (dimap, lcmap)
 import Data.Tuple (curry, snd)
 import Data.Tuple.Nested (type (/\), (/\))
 import Deku.Attribute (Attribute, AttributeValue(..), unsafeUnAttribute)
-import Deku.Core (DOMInterpret(..), Domable(..), Node(..), Nut, bus, dyn, insert_, remove, sendToPos)
-import Effect (Effect)
+import Deku.Core (DOMInterpret(..), Domable(..), Node(..), Nut, dyn, insert_)
 import FRP.Event (Event, Subscriber(..), keepLatest, makeLemmingEventO, mapAccum, memoize)
 import Prim.Int (class Compare)
 import Prim.Ordering (GT)
@@ -75,6 +77,8 @@ unsafeSetAttribute (DOMInterpret { setProp, setCb, unsetAttribute }) id atts =
     )
     (atts)
 
+-- | Used internally to create new [`Element`-s](https://developer.mozilla.org/en-US/docs/Web/API/Element).
+-- | Do not use this directly. Instead, use `unsafeCustomElement` from `Deku.DOM`.
 elementify
   :: forall element lock payload
    . String
@@ -119,6 +123,25 @@ elementify tag atts children = Node go
         runSTFn1 k (deleteFromCache { id: me })
         unsub
 
+-- | Creates a portal.
+-- |
+-- | Portals allow HTML elements to jump from one part of a page to another without being
+-- | torn down and recreated. This is useful for things like in-progress videos that are removed
+-- | from the top of the page and inset into a corner when you start scrolling (this is common on
+-- | educational websites like three.js journey).
+-- |
+-- | The first argument is a vector of elements that will be used as the portal's "source".
+-- | The second argument is a closure into which the vector is injected. The closure may use the
+-- | elements in the vector however it wants, but it must return a single element.
+-- | As the DOM cannot show a single element in multiple places, the portal will only show the
+-- | most recent emission of the element.
+-- |
+-- | Global portals have a global scope, meaning that their elements will persist for the lifetime
+-- | of a Deku application. In practice, you can have tens of thousands of global portals before
+-- | garbage collection starts to be an issue.
+-- |
+-- | For examples of portals in use, see the
+-- | [portals section](https://purescript-deku.netlify.app/core-concepts/portals) of the Deku guide.
 globalPortal
   :: forall n lock payload
    . Compare n Neg1 GT
@@ -137,6 +160,7 @@ globalPortal v c = Domable $ Bolson.globalPortalComplexComplex
   (map unwrap v)
   (dimap (map ((_ $ unit) >>> wrap)) unwrap c)
 
+-- | A variation of global portal that takes a single element instead of a vector of elements.
 globalPortal1
   :: forall lock payload
    . Domable lock payload
@@ -180,6 +204,7 @@ portalFlatten =
   , toElt: \(Node e) -> Element e
   }
 
+-- | A variation of portal that takes a single element instead of a vector of elements.
 portal1
   :: forall lock payload
    . Domable lock payload
@@ -192,6 +217,27 @@ portal1
   -> Domable lock payload
 portal1 i f = portal (singleton i) (lcmap (over _1 (index (Proxy :: _ 0))) f)
 
+-- | Creates a portal.
+-- |
+-- | Portals allow HTML elements to jump from one part of a page to another without being
+-- | torn down and recreated. This is useful for things like in-progress videos that are removed
+-- | from the top of the page and inset into a corner when you start scrolling (this is common on
+-- | educational websites like three.js journey).
+-- |
+-- | The first argument is a vector of elements that will be used as the portal's "source".
+-- | The second argument is a closure into which the vector is injected. The closure may use the
+-- | elements in the vector however it wants, but it must return a single element.
+-- | As the DOM cannot show a single element in multiple places, the portal will only show the
+-- | most recent emission of the element.
+-- |
+-- | Portals have a local scope, meaning that they can only be used inside their closure.
+-- | This is enforced by the `lock` type variable. Local portals are necessary when you are creating
+-- | hundreds of thousands of elements, in which case garbage collection and memory management
+-- | start to be an issue any you will need for them to be destroyed when their enclosing scope
+-- | is destroyed. This comes at the cost, however, of a slightly less ergonomic syntax.
+-- |
+-- | For examples of portals in use, see the
+-- | [portals section](https://purescript-deku.netlify.app/core-concepts/portals) of the Deku guide.
 portal
   :: forall n lock0 payload
    . Compare n Neg1 GT
@@ -215,6 +261,8 @@ portal a b = Domable $ Bolson.portalComplexComplex
   (map unwrap a)
   (lcmap (map (_ $ unit)) (coerce (curry b)))
 
+-- | Create a [`Text`](https://developer.mozilla.org/en-US/docs/Web/API/Text) node from
+-- | the emitted strings. Each emitted string replaces the previous string.
 text
   :: forall lock payload
    . Event String
@@ -244,9 +292,13 @@ text txt = Domable $ Element' $ Node go
         runSTFn1 k (deleteFromCache { id: me })
         unsub
 
+-- | Create a [`Text`](https://developer.mozilla.org/en-US/docs/Web/API/Text) node from
+-- | a string. The node is set immediately with the string and does not change.
 text_ :: forall lock payload. String -> Domable lock payload
 text_ txt = text (pure txt)
 
+-- | A low-level function that creates a Deku application.
+-- | In most situations this should not be used. Instead, use functions from `Deku.Toplevel`.
 deku
   :: forall payload
    . Web.DOM.Element
@@ -281,6 +333,12 @@ __internalDekuFlatten
   -> Event payload
 __internalDekuFlatten a b (Domable c) = Bolson.flatten portalFlatten a b c
 
+-- | Like `bindFlipped`, except instead of working with a monad, it dipts into an `Event`
+-- | and creates a `Domable`. This allows you to use an event to switch between different
+-- | bits of DOM. This is how a [Virtual DOM](https://en.wikipedia.org/wiki/Virtual_DOM) works
+-- | in its most basic, unoptimized form. As a result, `switcher`, while convenient, is inefficient
+-- | and should be used when the content needs to be replaced wholesale. For a more efficient
+-- | approach, see the `useDyn` hook.
 switcher
   :: forall a lock payload
    . (a -> Domable lock payload)
@@ -301,6 +359,7 @@ switcher f event = dyn $ keepLatest
 
 infixl 4 switcher as <$~>
 
+-- | A flipped version of `switcher`.
 switcherFlipped
   :: forall a lock payload
    . Event a
@@ -310,6 +369,7 @@ switcherFlipped = flip switcher
 
 infixl 1 switcherFlipped as <#~>
 
+-- | Inserts the Deku Nut when an event emits `true`, otherwise destroys the element.
 guard
   :: forall lock payload
    . Event Boolean
@@ -318,23 +378,6 @@ guard
 guard eb d = switcher
   (if _ then d else blank)
   eb
-
-ezDyn
-  :: forall lock payload
-   . ( Event
-         ( { remove :: Effect Unit, sendToPos :: Int -> Effect Unit }
-           -> Domable lock payload
-         )
-     )
-  -> Domable lock payload
-ezDyn e1 = dyn
-  ( e1 <#> \f1 -> keepLatest $ bus \setRm rm ->
-      keepLatest $ bus \setStp stp ->
-        (rm $> remove) <|> (stp <#> sendToPos) <|> pure
-          ( insert_
-              (unsafeCoerce (f1 { remove: setRm unit, sendToPos: setStp }))
-          )
-  )
 
 blank :: Nut
 blank = Domable $ BCore.envy empty
