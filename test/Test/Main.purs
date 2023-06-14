e383ffc85233fe9fa6c79a6221fd32fb8b7b1842
Mike Solomon <mike@meeshkan.com>
2023-06-14 15:43:13 +0300

    Changes useDyn to be more ergonomic (#98)
    * Starts messing with sending to position
    
    * Puts useEffect hook on separate track
    
    * Gets rid of insert_ and insert in tests

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -8,16 +8,16 @@ import Control.Monad.ST.Internal (ST)
 import Control.Plus (empty)
 import Data.Array ((..))
 import Data.Filterable (filter)
-import Data.Foldable (intercalate, oneOf, oneOfMap)
+import Data.Foldable (intercalate, oneOfMap)
 import Data.Tuple (Tuple(..), snd)
 import Data.Tuple.Nested (type (/\), (/\))
 import Deku.Attribute ((!:=), (:=))
 import Deku.Attributes (id_)
 import Deku.Control (blank, globalPortal1, portal1, switcher, text, text_, (<#~>))
-import Deku.Core (Nut, Hook, dyn, fixed, insert, insert_, sendToPos)
+import Deku.Core (Hook, Nut, fixed)
 import Deku.DOM as D
 import Deku.Do as Deku
-import Deku.Hooks (useEffect, useMemoized, useRef, useState, useState')
+import Deku.Hooks (useDyn, useDynAtBeginning, useDynAtEnd, useEffect, useMemoized, useRef, useState, useState', (//\\))
 import Deku.Lifecycle (onDidMount, onDismount, onWillMount)
 import Deku.Listeners (click, click_)
 import Deku.Pursx ((~~))
@@ -61,8 +61,8 @@ elementsInCorrectOrder = do
           ]
   D.div [ id_ "div0-0" ] (l 1)
 
-dynAppearsCorrectly :: Nut
-dynAppearsCorrectly = Deku.do
+dynAppearsCorrectlyAtBeginning :: Nut
+dynAppearsCorrectlyAtBeginning = Deku.do
   let
     counter :: forall a. Event a -> Event Int
     counter event = fold (\a _ -> a + 1) (-1) event
@@ -70,10 +70,24 @@ dynAppearsCorrectly = Deku.do
   D.div [ id_ "div0" ]
     [ text_ "foo"
     , D.span [ id_ "div1" ] [ text_ "bar" ]
-    , dyn
-        ( counter item <#> \i -> pure
-            $ insert_ (D.span [ id_ ("dyn" <> show i) ] [ text_ (show i) ])
-        )
+    , Deku.do
+        { value: i } <- useDynAtBeginning (counter item)
+        D.span [ id_ ("dyn" <> show i) ] [ text_ (show i) ]
+    , D.button [ id_ "incr", click_ (setItem unit) ] [ text_ "incr" ]
+    ]
+
+dynAppearsCorrectlyAtEnd :: Nut
+dynAppearsCorrectlyAtEnd = Deku.do
+  let
+    counter :: forall a. Event a -> Event Int
+    counter event = fold (\a _ -> a + 1) (-1) event
+  setItem /\ item <- useState'
+  D.div [ id_ "div0" ]
+    [ text_ "foo"
+    , D.span [ id_ "div1" ] [ text_ "bar" ]
+    , Deku.do
+        { value: i } <- useDynAtEnd (counter item)
+        D.span [ id_ ("dyn" <> show i) ] [ text_ (show i) ]
     , D.button [ id_ "incr", click_ (setItem unit) ] [ text_ "incr" ]
     ]
 
@@ -87,13 +101,11 @@ deeplyNestedPreservesOrder = Deku.do
     mydyn n = do
       let sn = show n
       fixed
-        [ dyn
-            ( counter item <#> \i -> pure
-                $ insert_
-                    if i == 1 then mydyn (n + 1)
-                    else D.span [ id_ ("dyn" <> sn <> "-" <> show i) ]
-                      [ text_ (sn <> "-" <> show i) ]
-            )
+        [ Deku.do
+            { value: i } <- useDynAtEnd (counter item)
+            if i == 1 then mydyn (n + 1)
+            else D.span [ id_ ("dyn" <> sn <> "-" <> show i) ]
+              [ text_ (sn <> "-" <> show i) ]
         , D.button [ id_ $ "incr-" <> sn, click_ (setItem unit) ]
             [ text_ $ "incr-" <> sn ]
         ]
@@ -113,14 +125,10 @@ sendsToPosition = Deku.do
   D.div [ id_ "div0" ]
     [ text_ "foo"
     , D.span [ id_ "div1" ] [ text_ "bar" ]
-    , dyn
-        ( oneOfMap pure [ 0, 1, 2, 3, 4 ] <#> \i ->
-            oneOf
-              [ pure $ insert_
-                  (D.span [ id_ ("dyn" <> show i) ] [ text_ (show i) ])
-              , if i == 3 then posIdx <#> sendToPos else empty
-              ]
-        )
+    , Deku.do
+        { value: i, sendTo } <- useDynAtEnd (oneOfMap pure [ 0, 1, 2, 3, 4 ])
+        useEffect (if i == 3 then posIdx else empty) sendTo
+        D.span [ id_ ("dyn" <> show i) ] [ text_ (show i) ]
     , D.button [ id_ "pos", click_ (setPosIdx 1) ]
         [ text_ "send to pos" ]
     ]
@@ -131,19 +139,15 @@ sendsToPositionFixed = Deku.do
   D.div [ id_ "div0" ]
     [ text_ "foo"
     , D.span [ id_ "div1" ] [ text_ "bar" ]
-    , dyn
-        ( oneOfMap pure [ 0, 1, 2, 3, 4 ] <#> \i ->
-            oneOf
-              [ pure $ insert_
-                  $ fixed
-                      [ D.span [ id_ ("dyn" <> show i <> "a") ]
-                          [ text_ (show i <> "a") ]
-                      , D.span [ id_ ("dyn" <> show i <> "a") ]
-                          [ text_ (show i <> "b") ]
-                      ]
-              , if i == 3 then posIdx <#> sendToPos else empty
-              ]
-        )
+    , Deku.do
+        { value: i, sendTo } <- useDynAtEnd (oneOfMap pure [ 0, 1, 2, 3, 4 ])
+        useEffect (if i == 3 then posIdx else empty) sendTo
+        fixed
+          [ D.span [ id_ ("dyn" <> show i <> "a") ]
+              [ text_ (show i <> "a") ]
+          , D.span [ id_ ("dyn" <> show i <> "a") ]
+              [ text_ (show i <> "b") ]
+          ]
     , D.button [ id_ "pos", click_ (setPosIdx 1) ]
         [ text_ "send to pos" ]
     ]
@@ -152,19 +156,15 @@ insertsAtCorrectPositions :: Nut
 insertsAtCorrectPositions = D.div [ id_ "div0" ]
   [ text_ "foo"
   , D.span [ id_ "div1" ] [ text_ "bar" ]
-  , dyn
+  , Deku.do
       -- if we just used insert_ here, it would go in
       -- linear order
       -- here, we scramble the order and make sure that the dyns
       -- are inserted in the scrambled order so that they read
       -- 0-1-2-3-4 from top to bottom
-      ( oneOfMap pure [ 3, 0, 4, 2, 1 ] <#> \i ->
-          oneOf
-            [ pure $ insert i
-                (D.span [ id_ ("dyn" <> show i) ] [ text_ (show i) ])
-            ]
-
-      )
+      { value: i } <- useDyn
+        ((identity //\\ _) <$> oneOfMap pure [ 3, 0, 4, 2, 1 ])
+      D.span [ id_ ("dyn" <> show i) ] [ text_ (show i) ]
   ]
 
 switcherWorksForCompositionalElements :: Nut
