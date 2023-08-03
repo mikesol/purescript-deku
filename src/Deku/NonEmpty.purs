802fe889a81efdd9b064a16d189c439051aa7301
Mike Solomon <mike@meeshkan.com>
2023-08-03 07:04:42 +0300

    Adds more nonempty functions

--- src/Deku/NonEmpty.purs
+++ src/Deku/NonEmpty.purs
@@ -7,6 +7,8 @@ import Data.Either (Either(..))
 import Data.Filterable as Filterable
 import Data.Maybe (Maybe(..))
 import Data.NonEmpty (NonEmpty, (:|))
+import Deku.Attribute (class Attr, Attribute, (:=))
+import FRP.Event (Event)
 
 rehead :: forall f a. a -> NonEmpty f a -> NonEmpty f a
 rehead hn (_ :| t) = hn :| t
@@ -49,3 +51,27 @@ apne
 apne (a' :| as') (b' :| bs') = a' b' :| (as' <*> bs')
 
 infixl 4 apne as <**>
+
+filterMapAttribute
+  :: forall e a
+   . Attr e a (Event a)
+  => Attr e a (NonEmpty Event a)
+  => a
+  -> (a -> Maybe a)
+  -> NonEmpty Event a
+  -> Attribute e
+filterMapAttribute a f ne = case filterMap f ne of
+  Left x -> a := x
+  Right y -> a := y
+
+filterAttribute
+  :: forall e a
+   . Attr e a (Event a)
+  => Attr e a (NonEmpty Event a)
+  => a
+  -> (a -> Boolean)
+  -> NonEmpty Event a
+  -> Attribute e
+filterAttribute a f ne = case filter f ne of
+  Left x -> a := x
+  Right y -> a := y
