7484bb5f04590b3824f8bac5514f0bef4261b4cf
Mike Solomon <mike@meeshkan.com>
2023-08-21 09:38:03 +0300

    Fixes codegen

--- deku/src/Deku/Pursx.purs
+++ deku/src/Deku/Pursx.purs
@@ -3,7 +3,7 @@ module Deku.Pursx where
 import Data.Reflectable (class Reflectable, reflectType)
 import Deku.Attribute (Attribute)
 import Deku.Core (Nut)
-import Deku.Pursx.Unsafe (unsafeMakePursx, unsafeMakePursx', class PursxToElement)
+import Deku.Pursx.Unsafe (class PursxToElement, unsafeMakePursx, unsafeMakePursx')
 import Deku.DOM (class TagToDeku)
 import FRP.Event (Event)
 import Prim.Boolean (False, True)
