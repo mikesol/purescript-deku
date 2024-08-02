6b2ac1ec6ee6d0217315707a1b718c805c6d753a
Mike Solomon <mike.solomon@hey.com>
2024-08-02 10:50:41 +0300

    Adds has element parent

--- deku-core/src/Deku/Internal/Ancestry.purs
+++ deku-core/src/Deku/Internal/Ancestry.purs
@@ -6,10 +6,13 @@ module Deku.Internal.Ancestry
   , fixed
   , toStringRepresentationInDOM
   , unsafeFakeAncestry
+  , hasElementParent
   , Ancestry
   ) where
 
 import Prelude
+
+import Data.String as String
 
 data DekuAncestry
   = Element Int DekuAncestry
@@ -18,7 +21,7 @@ data DekuAncestry
   | Fixed Int DekuAncestry
   | Root
 
-data Ancestry = RealAncestry { rep :: String, lineage :: DekuAncestry } | FakeAncestry { rep :: String }
+data Ancestry = RealAncestry { rep :: String, lineage :: DekuAncestry, hasElementParent :: Boolean } | FakeAncestry { rep :: String }
 
 instance Eq Ancestry where
   eq (RealAncestry a) (RealAncestry b) = a.rep == b.rep
@@ -32,30 +35,35 @@ instance Ord Ancestry where
   compare (RealAncestry a) (FakeAncestry b) = compare a.rep b.rep
   compare (FakeAncestry a) (RealAncestry b) = compare a.rep b.rep
 
+
+hasElementParent :: Ancestry -> Boolean
+hasElementParent (RealAncestry a) = a.hasElementParent
+hasElementParent (FakeAncestry { rep }) = String.contains (String.Pattern "e") rep
+
 root :: Ancestry
-root = RealAncestry { rep: "", lineage: Root }
+root = RealAncestry { rep: "", lineage: Root, hasElementParent: false }
 
 element :: Int -> Ancestry -> Ancestry
 element i (RealAncestry a) = RealAncestry
-  { rep: a.rep <> "e" <> show i, lineage: Element i a.lineage }
+  { rep: a.rep <> "e" <> show i, lineage: Element i a.lineage, hasElementParent: true }
 element i (FakeAncestry a) = FakeAncestry
   { rep: a.rep <> "e" <> show i  }
 
 dyn :: Int -> Ancestry -> Ancestry
 dyn i (RealAncestry a) = RealAncestry
-  { rep: a.rep <> "d" <> show i, lineage: Dyn i a.lineage }
+  { rep: a.rep <> "d" <> show i, lineage: Dyn i a.lineage, hasElementParent: a.hasElementParent  }
 dyn i (FakeAncestry a) = FakeAncestry
   { rep: a.rep <> "d" <> show i  }
 
 portal :: Int -> Ancestry -> Ancestry
 portal i (RealAncestry a) = RealAncestry
-  { rep: a.rep <> "p" <> show i, lineage: Portal i a.lineage }
+  { rep: a.rep <> "p" <> show i, lineage: Portal i a.lineage, hasElementParent: a.hasElementParent }
 portal i (FakeAncestry a) = FakeAncestry
   { rep: a.rep <> "p" <> show i }
 
 fixed :: Int -> Ancestry -> Ancestry
 fixed i (RealAncestry a) = RealAncestry
-  { rep: a.rep <> "f" <> show i, lineage: Fixed i a.lineage }
+  { rep: a.rep <> "f" <> show i, lineage: Fixed i a.lineage, hasElementParent: a.hasElementParent  }
 fixed i (FakeAncestry a) = FakeAncestry
   { rep: a.rep <> "f" <> show i  }
 
